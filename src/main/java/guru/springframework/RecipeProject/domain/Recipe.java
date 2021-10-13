6be35499e3fc5e7391acb8eed58fc7f9010285df
Andres Lerner <38118136+AndresL99@users.noreply.github.com>
2021-10-13 09:48:30 -0300

    Jpa Relationships

--- src/main/java/guru/springframework/RecipeProject/domain/Recipe.java
+++ src/main/java/guru/springframework/RecipeProject/domain/Recipe.java
@@ -18,6 +18,9 @@ public class Recipe
     private String url;
     private String directions;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient>ingredients;
 
@@ -26,6 +29,27 @@ public class Recipe
 
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn( name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category>categories;
+
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public Long getId() {
         return id;
@@ -105,5 +129,13 @@ public class Recipe
 
     public void setNote(Note note) {
         this.note = note;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
