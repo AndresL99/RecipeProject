3828b296f5930bf44b3c2c78d0514a35712663b8
Andres Lerner <38118136+AndresL99@users.noreply.github.com>
2021-12-09 11:38:24 -0300

    Bootstrap and details

--- src/main/java/guru/springframework/RecipeProject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/RecipeProject/repositories/UnitOfMeasureRepository.java
@@ -3,7 +3,9 @@ package guru.springframework.RecipeProject.repositories;
 import guru.springframework.RecipeProject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>
 {
-
+    Optional<UnitOfMeasure>findByDescription(String description);
 }
